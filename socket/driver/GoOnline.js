66a5196080864db430926d33b403c9615f292014
MAHMOUD ELASHMAWY <elashmawydev@gmail.com>
2021-04-11 12:04:05 +0200

    some edits to GoOnline

--- socket/driver/GoOnline.js
+++ socket/driver/GoOnline.js
@@ -18,99 +18,100 @@ const { checkForOrderRequest } = require("../../helpers");
 let EventLocks = new Map();
 
 module.exports = (io, socket) => {
-  socket.on(
-    "GoOnline",
-    async ({ driverId, status, token }) => {
-      /*
-       * Start the Event Locker from here
-       */
+  socket.on("GoOnline", async ({ driverId, status, token }) => {
+    /*
+     * Start the Event Locker from here
+     */
 
-      if (!EventLocks.has(driverId)) EventLocks.set(driverId, new Mutex());
+    if (!EventLocks.has(driverId)) EventLocks.set(driverId, new Mutex());
 
-      const releaseEvent = await EventLocks.get(driverId).acquire();
+    const releaseEvent = await EventLocks.get(driverId).acquire();
+
+    /***************************************************/
+
+    try {
+      console.log(
+        `GoOnline Event Called, driver id: ${driverId}, ${
+          status == 1 ? "online" : "offline"
+        }`
+      );
+
+      driverId = parseInt(driverId);
+      /********************************************************/
+
+      //Check if token is valid
+      let driverSearch = await DriverModel.findOne({
+        driverId,
+        accessToken: token,
+      });
+
+      if (!driverSearch) {
+        return socket.emit("GoOnline", {
+          status: false,
+          isAuthorize: false,
+          isOnline: false,
+          message: "You are not authorized",
+        });
+      }
+
+      /******************************************************/
+      //Add driver to socket
+      drivers.set(driverId, socket.id);
+      //Remove from the disconnect interval
+      disconnectInterval.delete(driverId);
+
+      console.log(drivers);
+
+      /******************************************************/
+
+      //Search for busy orders
+      let busyOrders = await OrderModel.countDocuments({
+        "master.statusId": { $in: [3, 4] },
+        "master.driverId": driverId,
+      });
 
       /***************************************************/
 
-      try {
-        console.log(
-          `GoOnline Event Called, driver id: ${driverId}, ${
-            status == 1 ? "online" : "offline"
-          }`
-        );
-
-        driverId = parseInt(driverId);
-        /********************************************************/
-
-        //Check if token is valid
-        let driverSearch = await DriverModel.findOne({
-          driverId,
-          accessToken: token,
-        });
-
-        if (!driverSearch) {
-          return socket.emit("GoOnline", {
-            status: false,
-            isAuthorize: false,
-            isOnline: false,
-            message: "You are not authorized",
-          });
-        }
-
-        /******************************************************/
-        //Add driver to socket
-        drivers.set(driverId, socket.id);
-        //Remove from the disconnect interval
-        disconnectInterval.delete(driverId);
-
-        console.log(drivers);
-
-        /******************************************************/
-
-        //Search for busy orders
-        let busyOrders = await OrderModel.find({
-          "master.statusId": { $in: [3, 4] },
-          "master.driverId": driverId,
-        });
-
-        /***************************************************/
-
-        let isOnline = status == 1 ? true : false;
-        if (busyOrders.length > 0 && status == 1) isOnline = true;
-        if (busyOrders.length > 0 && status == 2) isOnline = true;
-        if (busyOrders.length == 0 && status == 1) isOnline = true;
-        if (busyOrders.length == 0 && status == 2) isOnline = false;
-
-        //Update the driver
-        await DriverModel.updateOne(
-          { driverId },
-          {
-            $set: {
-              isOnline,
-              isBusy: busyOrders.length > 0 ? true : false,
-            },
-          }
-        );
-        /***************************************************/
-        //Emit GoOnline with updated status
-        socket.emit("GoOnline", {
-          status: true,
-          isAuthorize: true,
-          isOnline,
-          message: `The driver is set to ${isOnline ? "online" : "offline"}`,
-        });
-
-        /***************************************************/
-      } catch (e) {
-        Sentry.captureException(e);
-
-        console.log(`Error in GoOnline, error: ${e.message}`);
-        return socket.emit("GoOnline", {
-          status: false,
-          message: `Error in GoOnline, error: ${e.message}`,
-        });
-      } finally {
-        releaseEvent(); //Stop event locker
+      let isOnline = status == 1 ? true : false;
+      let isForced = false;
+      if (busyOrders > 0 && status == 1) isOnline = true;
+      if (busyOrders > 0 && status == 2) {
+        isOnline = true;
+        isForced = true;
       }
+      if (busyOrders == 0 && status == 1) isOnline = true;
+      if (busyOrders == 0 && status == 2) isOnline = false;
+
+      //Update the driver
+      await DriverModel.updateOne(
+        { driverId },
+        {
+          $set: {
+            isOnline,
+            isBusy: busyOrders > 0 ? true : false,
+          },
+        }
+      );
+      /***************************************************/
+      //Emit GoOnline with updated status
+      socket.emit("GoOnline", {
+        status: !isForced,
+        isAuthorize: true,
+        isOnline,
+        message: `The driver is set to ${isOnline ? "online" : "offline"}`,
+      });
+
+      /***************************************************/
+    } catch (e) {
+      Sentry.captureException(e);
+
+      console.log(`Error in GoOnline, error: ${e.message}`);
+      return socket.emit("GoOnline", {
+        status: false,
+        message: `Error in GoOnline, error: ${e.message}`,
+      });
+    } finally {
+      releaseEvent(); //Stop event locker
     }
-  );
+  });
 };
