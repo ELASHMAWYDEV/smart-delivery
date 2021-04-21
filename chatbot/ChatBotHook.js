873c6b33352d6ffbddfba53c397adb6bc270897f
ELASHMAWYDEV <elashmawydev@gmail.com>
2021-04-21 15:52:33 +0200

    Some edits to Chat Hook

--- chatbot/ChatBotHook.js
+++ chatbot/ChatBotHook.js
@@ -1,9 +1,99 @@
+const Sentry = require('@sentry/node');
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const stringSimilarity = require('string-similarity');
+const { CHAT_API_SEND_MESSAGE, CHAT_API_TYPING, CHAT_MOBILE_PHONE } = require('../globals');
+const QUESTIONS = require('./Questions.json');
 
 router.post('/', async (req, res) => {
-  console.log("chat hook",req.body);
-	res.json(req.body);
+	try {
+		const { messages } = req.body;
+
+		if (!messages || messages.length == 0)
+			return res.json({ status: false, message: 'Messages array in body is empty' });
+
+		for (let message of messages) {
+			//Get the message data
+			let { body, fromMe, author, chatId, type, senderName } = message;
+
+			await axios.post(CHAT_API_TYPING, {
+				chatId: chatId,
+				on: true,
+				duration: 5,
+				phone: author.split('@')[0],
+			});
+
+			/****************------Validation START-----*************************/
+			//From a group --> don't respond
+			if (chatId.includes('-'))
+				return res.json({ status: false, message: 'Sorry, this message was sent from a group' });
+
+			//From me --> but not to me (for testing)
+			if (author.split('@')[0] != CHAT_MOBILE_PHONE && fromMe == true)
+				return res.json({ status: false, message: 'Sorry, you sent this message by your self' });
+
+			/*************************************************/
+
+			//Location handling
+			if (type == 'location') {
+				return await axios.post(CHAT_API_SEND_MESSAGE, {
+					chatId: chatId,
+					body: `شكرا لمشاركة موقعك معنا ، سوف نقوم بتسجيله لدينا\nموقعك هو ${body.split(';')}`,
+				});
+			}
+
+			let { bestMatch, bestMatchIndex } = stringSimilarity.findBestMatch(
+				body,
+				QUESTIONS.map((q) => q.Q)
+			);
+
+			if (bestMatch.rating > 0.6) {
+				return await axios.post(CHAT_API_SEND_MESSAGE, {
+					chatId: chatId,
+					body: QUESTIONS[bestMatchIndex].R,
+				});
+			}
+
+			return await axios.post(CHAT_API_SEND_MESSAGE, {
+				chatId: chatId,
+				body: `هذه تجربة للشات بوت ، نعتذر اذا وصلتك هذه الرسالة بالخطأ ، لأننا في مرحلة التطوير الأن\nيمكنك ارسال أحد هذه الجمل لكي نرد عليك\n1- مرحبا\n2- كيف حالك\n3- أو يمكنك مشاركة الموقع`,
+			});
+		}
+
+		res.json({ status: true, message: 'Done !' });
+	} catch (e) {
+		Sentry.captureException(e);
+		console.log(`Error in ChatBotHook: ${e.message}`, e);
+		if (!res.headersSent) {
+			return res.json({ status: false, message: `Error in ChatBotHook: ${e.message}` });
+		}
+	}
 });
 
+/*
+  ==>Example of messages list --> req.body.messages
+  messages: [
+    {
+      id: 'true_201064544529@c.us_BDFEC1BEA0845E9B6CFE245D00A24A2E',
+      body: 'هلا',
+      fromMe: true,
+      self: 0,
+      isForwarded: 0,
+      author: '201064544529@c.us',
+      time: 1619006194,
+      chatId: '201064544529@c.us',
+      messageNumber: 17607,
+      type: 'chat',
+      senderName: 'MAHMOUD ELASHMAWY',
+      caption: null,
+      quotedMsgBody: null,
+      quotedMsgId: null,
+      quotedMsgType: null,
+      chatName: '+20 106 454 4529'
+    }
+  ]
+
+
+*/
 module.exports = router;
--- globals.js
+++ globals.js
@@ -1,20 +1,20 @@
-require("dotenv/config");
-const { Mutex } = require("async-mutex");
+require('dotenv/config');
+const { Mutex } = require('async-mutex');
 
 //Api Url
 module.exports.API_URI =
-  process.env.NODE_ENV == "development"
-    ? process.env.API_URI_DEVELOPMENT
-    : process.env.NODE_ENV == "pre-prod"
-    ? process.env.API_URI_PRE_PROD
-    : process.env.NODE_ENV == "production" && process.env.API_URI_PRODUCTION;
+	process.env.NODE_ENV == 'development'
+		? process.env.API_URI_DEVELOPMENT
+		: process.env.NODE_ENV == 'pre-prod'
+		? process.env.API_URI_PRE_PROD
+		: process.env.NODE_ENV == 'production' && process.env.API_URI_PRODUCTION;
 //Mongo URI
 module.exports.DB_URI =
-  process.env.NODE_ENV == "development"
-    ? process.env.DB_URI_DEVELOPMENT
-    : process.env.NODE_ENV == "pre-prod"
-    ? process.env.DB_URI_PRE_PROD
-    : process.env.NODE_ENV == "production" && process.env.DB_URI_PRODUCTION;
+	process.env.NODE_ENV == 'development'
+		? process.env.DB_URI_DEVELOPMENT
+		: process.env.NODE_ENV == 'pre-prod'
+		? process.env.DB_URI_PRE_PROD
+		: process.env.NODE_ENV == 'production' && process.env.DB_URI_PRODUCTION;
 
 //Global variables
 module.exports.clients = new Map(); //Restaurants socket ids'
@@ -28,7 +28,11 @@ module.exports.disconnectInterval = new Map();
 module.exports.busyDrivers = new Map(); //Hold all drivers that are currently busy with thier branch id & orders ids[]
 
 //API's
-module.exports.GOOGLE_MAPS_KEY = process.env.GOOGLE_MAPS_KEY || "";
-module.exports.FIREBASE_URI =
-  process.env.FIREBASE_URI || "https://super-1231e.firebaseio.com";
-module.exports.API_SECRET_KEY = process.env.API_SECRET_KEY || "randomtoken";
+module.exports.GOOGLE_MAPS_KEY = process.env.GOOGLE_MAPS_KEY || '';
+module.exports.FIREBASE_URI = process.env.FIREBASE_URI || 'https://super-1231e.firebaseio.com';
+module.exports.API_SECRET_KEY = process.env.API_SECRET_KEY || 'randomtoken';
+
+//Chat API
+module.exports.CHAT_API_SEND_MESSAGE = process.env.CHAT_API_URI + '/sendMessage?token=' + process.env.CHAT_API_TOKEN;
+module.exports.CHAT_API_TYPING = process.env.CHAT_API_URI + '/typing?token=' + process.env.CHAT_API_TOKEN;
+module.exports.CHAT_MOBILE_PHONE = process.env.CHAT_MOBILE_PHONE;
