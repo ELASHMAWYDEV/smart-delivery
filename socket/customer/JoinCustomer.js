0ac8b6f2fb1927f231ba10470d81dc79f39b33e9
ELASHMAWYDEV <elashmawydev@gmail.com>
2021-04-26 03:28:45 +0200

    added some data to JoinCustomer

--- socket/customer/JoinCustomer.js
+++ socket/customer/JoinCustomer.js
@@ -1,52 +1,55 @@
-const Sentry = require("@sentry/node");
-const OrderModel = require("../../models/Order");
-const DriverModel = require("../../models/Driver");
-const { customers } = require("../../globals");
+const Sentry = require('@sentry/node');
+const OrderModel = require('../../models/Order');
+const DriverModel = require('../../models/Driver');
+const { customers } = require('../../globals');
 
 module.exports = (io, socket) => {
-  socket.on("JoinCustomer", async ({ orderId }) => {
-    try {
-      console.log(`New Customer joined: ${orderId}`);
-      customers.set(parseInt(orderId), socket.id);
+	socket.on('JoinCustomer', async ({ orderId }) => {
+		try {
+			console.log(`New Customer joined: ${orderId}`);
+			customers.set(parseInt(orderId), socket.id);
 
-      const orderSearch = await OrderModel.findOne({
-        "master.orderId": orderId,
-      });
+			const orderSearch = await OrderModel.findOne({
+				'master.orderId': orderId,
+			});
 
-      if (!orderSearch) {
-        return socket.emit("JoinCustomer", {
-          status: false,
-          message: `There is no order with id #${orderId}`,
-        });
-      }
+			if (!orderSearch) {
+				return socket.emit('JoinCustomer', {
+					status: false,
+					message: `There is no order with id #${orderId}`,
+				});
+			}
 
-      if (!orderSearch.master.driverId) {
-        return socket.emit("JoinCustomer", {
-          status: false,
-          message: "We can't get the driver's location at the moment",
-        });
-      }
+			if (!orderSearch.master.driverId) {
+				return socket.emit('JoinCustomer', {
+					status: false,
+					message: "We can't get the driver's location at the moment",
+				});
+			}
 
-      //Get the driver location
-      const driverSearch = await DriverModel.findOne({
-        driverId: orderSearch.master.driverId,
-      });
+			//Get the driver location
+			const driverSearch = await DriverModel.findOne({
+				driverId: orderSearch.master.driverId,
+			});
 
-      return socket.emit("JoinCustomer", {
-        status: true,
-        driverLocation: {
-          lng: driverSearch.location.coordinates[0],
-          lat: driverSearch.location.coordinates[1],
-        },
-      });
-    } catch (e) {
-      Sentry.captureException(e);
+			return socket.emit('JoinCustomer', {
+				status: true,
+				driverData: {
+					lng: driverSearch.location.coordinates[0],
+					lat: driverSearch.location.coordinates[1],
+					driverNameAr: driverSearch.driverNameAr,
+					driverNameEn: driverSearch.driverNameEn,
+					phoneNumber: driverSearch.phoneNumber,
+				},
+			});
+		} catch (e) {
+			Sentry.captureException(e);
 
-      console.log(`Error in JoinCustomer event, ${e.message}`);
-      socket.emit("JoinCustomer", {
-        status: false,
-        message: `Error in JoinCustomer event: ${e.message}`,
-      });
-    }
-  });
+			console.log(`Error in JoinCustomer event, ${e.message}`);
+			socket.emit('JoinCustomer', {
+				status: false,
+				message: `Error in JoinCustomer event: ${e.message}`,
+			});
+		}
+	});
 };
