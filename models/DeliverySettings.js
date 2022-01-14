555a50568c83bb81e21da3fa69ab54345e0d3dec
MAHMOUD ELASHMAWY <elashmawydev@gmail.com>
2022-01-14 20:48:47 +0200

    Added distance check between customers for drivers on way

--- models/DeliverySettings.js
+++ models/DeliverySettings.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
 
-const globalIntervalSchema = new mongoose.Schema({
-  index: Number,
-  maxDistance: Number,
-});
-
 const deliverySettingsSchema = new mongoose.Schema({
   timerSeconds: {
     type: Number,
@@ -14,14 +9,11 @@ const deliverySettingsSchema = new mongoose.Schema({
     type: Number,
     default: 20,
   },
-  globalIntervals: {
-    type: [globalIntervalSchema],
+  maxDistanceBetweenCustomers: {
+    type: Number,
+    default: 20,
   },
 });
 
-const DeliverySettings = mongoose.model(
-  "DeliverySettings",
-  deliverySettingsSchema,
-  "deliverySettings"
-);
+const DeliverySettings = mongoose.model("DeliverySettings", deliverySettingsSchema, "deliverySettings");
 module.exports = DeliverySettings;
--- models/Order.js
+++ models/Order.js
@@ -2,47 +2,52 @@ const mongoose = require("mongoose");
 const { Number, Boolean, String } = mongoose.Schema.Types;
 
 const pointSchema = new mongoose.Schema({
-	type: {
-		type: String,
-		enum: ["Point"],
-		required: true,
-		default: "Point",
-	},
-	coordinates: {
-		type: [Number],
-		required: true,
-	},
+  type: {
+    type: String,
+    enum: ["Point"],
+    required: true,
+    default: "Point",
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
 });
 
 const masterSchema = new mongoose.Schema({
-	createDate: {
-		type: Date,
-		default: Date.now(),
-	},
-	orderId: Number,
-	branchId: Number,
-	clientId: { type: Number, default: null },
-	driverId: Number,
-	branchNameAr: String,
-	branchNameEn: String,
-	branchAddress: String,
-	receiverAddress: String,
-	receiverName: String,
-	receiverMobile: String,
-	receiverDistance: Number,
-	branchLogo: String,
-	paymentTypeEn: String,
-	paymentTypeAr: String,
-	deliveryPriceEn: String,
-	deliveryPriceAr: String,
-	branchDistance: { type: Number, default: 1.5 },
-	branchLocation: {
-		type: pointSchema,
-		index: "2dsphere",
-		required: true,
-	},
-	//1 ==> created, 2 ==> not found, 3 ==> accept, 4 ==> received, 5 ==> delivered, 6 ==> canceled
-	statusId: { type: Number, default: 1 },
+  createDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  orderId: Number,
+  branchId: Number,
+  clientId: { type: Number, default: null },
+  driverId: Number,
+  branchNameAr: String,
+  branchNameEn: String,
+  branchAddress: String,
+  receiverAddress: String,
+  receiverName: String,
+  receiverMobile: String,
+  receiverDistance: Number,
+  receiverLocation: {
+    type: pointSchema,
+    index: "2dsphere",
+    required: true,
+  },
+  branchLogo: String,
+  paymentTypeEn: String,
+  paymentTypeAr: String,
+  deliveryPriceEn: String,
+  deliveryPriceAr: String,
+  branchDistance: { type: Number, default: 1.5 },
+  branchLocation: {
+    type: pointSchema,
+    index: "2dsphere",
+    required: true,
+  },
+  //1 ==> created, 2 ==> not found, 3 ==> accept, 4 ==> received, 5 ==> delivered, 6 ==> canceled
+  statusId: { type: Number, default: 1 },
 });
 
 // const itemSchema = new mongoose.Schema({
@@ -52,52 +57,52 @@ const masterSchema = new mongoose.Schema({
 // });
 
 const orderDriversSchema = new mongoose.Schema({
-	orderId: {
-		type: Number,
-	},
-	driverId: {
-		type: Number,
-	},
-	requestStatus: {
-		// 1 ==> accept, 2 ==> reject, 3 ==> ignore , 4 ==> noCatch, 5 ==> noCatch & seen by notification
-		type: Number,
-	},
-	location: {
-		type: pointSchema,
-		required: true,
-		index: "2dsphere",
-	},
-	actionDate: {
-		type: Date,
-	},
-	rangeIndex: {
-		type: Number,
-		default: 1,
-	},
-	estimatedDriverDistance: {
-		type: Number,
-		default: 0, //temp
-	},
-	estimatedDriverDuration: {
-		type: Number,
-		default: 0,
-	},
-	timeSent: {
-		type: Number,
-		default: new Date().getTime(),
-	},
-	isSeenOrder: {
-		type: Boolean,
-		default: false,
-	},
+  orderId: {
+    type: Number,
+  },
+  driverId: {
+    type: Number,
+  },
+  requestStatus: {
+    // 1 ==> accept, 2 ==> reject, 3 ==> ignore , 4 ==> noCatch, 5 ==> noCatch & seen by notification
+    type: Number,
+  },
+  location: {
+    type: pointSchema,
+    required: true,
+    index: "2dsphere",
+  },
+  actionDate: {
+    type: Date,
+  },
+  rangeIndex: {
+    type: Number,
+    default: 1,
+  },
+  estimatedDriverDistance: {
+    type: Number,
+    default: 0, //temp
+  },
+  estimatedDriverDuration: {
+    type: Number,
+    default: 0,
+  },
+  timeSent: {
+    type: Number,
+    default: new Date().getTime(),
+  },
+  isSeenOrder: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const orderSchema = new mongoose.Schema({
-	master: masterSchema,
-	// items: [itemSchema],
-	driversFound: {
-		type: [orderDriversSchema],
-	},
+  master: masterSchema,
+  // items: [itemSchema],
+  driversFound: {
+    type: [orderDriversSchema],
+  },
 });
 
 const Order = mongoose.model("Order", orderSchema, "orders");
